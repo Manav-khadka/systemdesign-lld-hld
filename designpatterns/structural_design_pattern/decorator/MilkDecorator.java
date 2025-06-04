8189f343eba677aa41a4c45313a6596e60b3340c
manav <manavkhadka2004@gmail.com>
2025-06-04 23:03:33 +0530

    Add MilkDecorator class to enhance Coffee with milk functionality

---
+++ designpatterns/structural_design_pattern/decorator/MilkDecorator.java
@@ -0,0 +1,17 @@
+package designpatterns.structural_design_pattern.decorator;
+public class MilkDecorator extends CoffeeDecorator {
+    public MilkDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Milk";
+    }
+
+    @Override
+    public double cost() {
+        return coffee.cost() + 1.0;
+    }
+}
+
