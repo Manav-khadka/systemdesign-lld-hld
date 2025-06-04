8af9e7c70b2d48026fa5e9e1c660c9155b7d4ed1
manav <manavkhadka2004@gmail.com>
2025-06-04 23:03:38 +0530

    Add SugarDecorator class to enhance Coffee with sugar functionality

---
+++ designpatterns/structural_design_pattern/decorator/SugarDecorator.java
@@ -0,0 +1,17 @@
+package designpatterns.structural_design_pattern.decorator;
+
+public class SugarDecorator extends CoffeeDecorator {
+    public SugarDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Sugar";
+    }
+
+    @Override
+    public double cost() {
+        return coffee.cost() + 0.5;
+    }
+}
